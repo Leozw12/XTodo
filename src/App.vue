f0c636395408b418394072d0d57e3a1b4b733b66
LxYx <1539357146@qq.com>
2023-08-25 00:00:04 +0800

    Update app

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,56 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+    <div id="app">
+        <el-container style="height: 100%; border: 1px solid #eee;">
+            <el-aside width="200px" style="background-color: rgb(238, 241, 246);">
+                <el-menu router>
+                    <el-menu-item index="/list">
+                        <i class="el-icon-menu"></i>
+                        <span slot="title">待办列表</span>
+                    </el-menu-item>
+                    <el-menu-item index="/add">
+                        <i class="el-icon-circle-plus-outline"></i>
+                        <span slot="title">添加待办</span>
+                    </el-menu-item>
+                </el-menu>
+            </el-aside>
+            <el-container>
+                <el-main>
+                    <router-view></router-view>
+                </el-main>
+            </el-container>
+        </el-container>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+    import {mapActions} from "vuex"
+    export default {
+        name: 'App',
+        components: {},
+        methods: {
+            ...mapActions(["initList"])
+        },
+        created() {
+            this.initList()
+        }
+    }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+    * {
+        margin: 0;
+        padding: 0;
+    }
+    
+    #app {
+        height: 100%;
+    }
+/*    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    } */
 </style>
